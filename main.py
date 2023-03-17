ba8b25579265936fe9fe6b349aec01f1b8b77ece
RstProfessional <rsantos.professional@gmail.com>
2023-03-17 18:45:34 +0000

    add functionality

--- main.py
+++ main.py
@@ -24,7 +24,20 @@ CHROME_PATH = os.getenv("CHROME_PATH")
 service = Service(executable_path=CHROME_PATH)
 driver = webdriver.Chrome(service=service)
 
-URL = "https://en.wikipedia.org/wiki/Main_Page"
+URL = 'https://web.archive.org/web/20220120120408/http://secure-retreat-92358.herokuapp.com/'
 
 #getting site
 driver.get(URL)
+
+first_name = driver.find_element(By.NAME, 'fName')
+last_name = driver.find_element(By.NAME, 'lName')
+email = driver.find_element(By.NAME, 'email')
+sign_up = driver.find_element(By.XPATH,'/html/body/form/button')
+
+first_name.send_keys('Rodolfo')
+last_name.send_keys('Santos')
+email.send_keys('rsantos.professional@gmail.com')
+sign_up.send_keys(Keys.ENTER)
+
+
+time.sleep(10)
